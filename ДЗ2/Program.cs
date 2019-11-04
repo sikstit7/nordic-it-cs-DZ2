60e8d45a3868bab986293762f68af1f550b4fbf3
Пользователь <kari.inago@icloud.com>
2019-11-04 13:02:42 +0300

    Домашнее задание 2

---
+++ ДЗ2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ДЗ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите первое число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("сложение");
+            var resultat = a + b;
+            Console.WriteLine("ответ равен:" + resultat);
+            Console.WriteLine("разница");
+            var resultat1 = a - b;
+            Console.WriteLine("ответ равен:" + resultat1);
+            Console.WriteLine("произведение");
+            var resultat2 = a * b;
+            Console.WriteLine("ответ равен:" + resultat2);
+            Console.ReadKey();
+
+        }
+    }
+}
